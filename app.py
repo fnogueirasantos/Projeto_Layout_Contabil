b16eba3aa8e0de8f6c0a6ddaec5ea70be4fa528a
Felipe Nogueira <fenogueirasantos@gmail.com>
2023-09-13 13:53:36 -0300

    Modificacoes1309

--- app.py
+++ app.py
@@ -4,14 +4,18 @@ import time
 import operacao
 import csv
 import datetime
-import pandas as pd
 import os
+import warnings
+warnings.filterwarnings("ignore")
 # Defina a variável de ambiente PYTHONUTF8 para o encoding padrão UTF-8
 os.environ["PYTHONUTF8"] = "1"
 
-st.title('Formatação de Layout de Importação Contábil')
+st.title('Layout de Importação Contábil')
 
-uploaded_file = st.file_uploader("Carregue o Arquivo:")
+# Crie um expander com um rótulo
+with st.expander("IMPOSTOS"):
+    uploaded_file = st.file_uploader("Carregue o Arquivo:",key="impostos")
+
 if uploaded_file is not None:
     try:
         df_opcoes = pd.read_excel(uploaded_file,dtype={'Centro de Custo': str})
@@ -19,11 +23,7 @@ if uploaded_file is not None:
     except Exception as e:
         st.error(f"Erro ao carregar os dados!!")
     time.sleep(1)
-
-if uploaded_file is not None:
-    df_opcoes = pd.read_excel(uploaded_file,dtype={'Centro de Custo': str})
-    opcoes = sorted(list(df_opcoes['Coligada'].unique()))
-    opcao_coligada = st.selectbox('Selecione a Coligada',(opcoes))
+    opcao_sheet = st.selectbox('Selecione o Conceito:',('1º Conceito', '2º Conceito','3º Conceito'))
     st.subheader('Definições de Cabeçalho do Arquivo')
     number = st.number_input('Código do Lote', value = 2023, key='int')
     title = st.text_input('Título do Lançamento', 'Importacao de lancamento contabil manual')
@@ -32,28 +32,75 @@ if uploaded_file is not None:
     if select_data:
         data_formatada = select_data.strftime("%d/%m/%Y")
     cabecalho = ['M', number, title, data_formatada]
-    st.text(f"Coligada Selecionada: {opcao_coligada}")
     st.text(f"Cabeçalho: {cabecalho}")
-    try:
-        df_filtro = pd.read_excel(uploaded_file,dtype={'Centro de Custo': str})
-        df_filtro = df_filtro.query(f'Coligada == {opcao_coligada}')
-        df = operacao.transforma_dados(df_filtro,cabecalho)
-    except Exception as e:
-        st.error(f"Erro ao carregar os dados: {e}")
+    if st.button("Transformar Dados"):
+        try:
+            # Mostra o spinner enquanto os dados estão sendo carregados e processados
+            with st.spinner("Processando os dados..."):
+                time.sleep(4)
+                df_filtro = pd.read_excel(uploaded_file, dtype={'Centro de Custo': str}, sheet_name=f'{opcao_sheet}')
+                zip_file_path = operacao.transforma_dados_e_cria_zip(df_filtro, cabecalho)
+        except Exception as e:
+            st.error(f"Erro ao carregar os dados: {e}")
+        else:
+            # Use a função st.download_button para permitir o download do arquivo ZIP
+            with open(zip_file_path, 'rb') as f:
+                zip_data = f.read()
+            st.download_button(
+                label="Clique para baixar o arquivo ZIP",
+                data=zip_data,
+                key="dados_download.zip",
+                file_name=f"Import_{opcao_sheet}.zip",
+                mime="application/zip"
+            )
 else:
     pass
-if uploaded_file is not None:
-    if st.button('Gerar Layout de Importação') and df is not None:
-        with st.spinner('Transformação de dados em Progresso...'):
-            try:
-                time.sleep(3)
-                st.success('Feito! O Layout está pronto para Download.')
-            except Exception as e:
-                st.error(f"Erro durante a transformação dos dados: {e}")
-        if df is not None:
-            st.download_button(
-                label="Download data as CSV",
-                data=open(df, 'rb').read(),
-                file_name=f'import_Colig_{opcao_coligada}.csv',
-                mime='text/csv',
-            )
+
+st.divider()
+
+# Você também pode aninhar expanders
+with st.expander("LAYOUT GERAL"):
+    uploaded_file2 = st.file_uploader("Carregue o Arquivo:",key="geral")
+
+    if uploaded_file2 is not None:
+        try:
+            df_opcoes = pd.read_excel(uploaded_file2,dtype={'Centro de Custo': str})
+            st.write('Dados Carregados com Sucesso!!')
+        except Exception as e:
+            st.error(f"Erro ao carregar os dados!!")
+        time.sleep(1)
+        opcoes = sorted(list(df_opcoes['Coligada'].unique()))
+        opcao_coligada = st.selectbox('Selecione a Coligada',(opcoes))
+        st.subheader('Definições de Cabeçalho do Arquivo')
+        number = st.number_input('Código do Lote', value = 2023, key='int')
+        title = st.text_input('Título do Lançamento', 'Importacao de lancamento contabil manual')
+        select_data = st.date_input("Data de Inclusão:")
+        # Verifique se a data foi selecionada
+        if select_data:
+            data_formatada = select_data.strftime("%d/%m/%Y")
+        cabecalho = ['M', number, title, data_formatada]
+        st.text(f"Coligada Selecionada: {opcao_coligada}")
+        st.text(f"Cabeçalho: {cabecalho}")
+        try:
+            df_filtro = pd.read_excel(uploaded_file2,dtype={'Centro de Custo': str})
+            df_filtro = df_filtro.query(f'Coligada == {opcao_coligada}')
+            df = operacao.transforma_dados(df_filtro,cabecalho)
+        except Exception as e:
+            st.error(f"Erro ao carregar os dados: {e}")
+    else:
+        pass
+    if uploaded_file2 is not None:
+        if st.button('Gerar Layout de Importação') and df is not None:
+            with st.spinner('Transformação de dados em Progresso...'):
+                try:
+                    time.sleep(3)
+                    st.success('Feito! O Layout está pronto para Download.')
+                except Exception as e:
+                    st.error(f"Erro durante a transformação dos dados: {e}")
+            if df is not None:
+                st.download_button(
+                    label="Download data as CSV",
+                    data=open(df, 'rb').read(),
+                    file_name=f'import_Colig_{opcao_coligada}.csv',
+                    mime='text/csv',
+                )
--- operacao.py
+++ operacao.py
@@ -1,5 +1,52 @@
 import csv
-import pandas as pd
+import zipfile
+import os
+import warnings
+warnings.filterwarnings("ignore")
+
+def transforma_dados_e_cria_zip(df,cabecalho):
+    df = df.dropna()
+    df['Partida'] = '*P'
+    df['Centro de Custo'] = df['Centro de Custo'].astype('str')
+    df['Nº Documento'] = ''
+    df['Conta de Contra Partida'] = ''
+    df['Coligada'] = df['Coligada'].astype('int')
+    df['Filial'] = df['Filial'].astype('int')
+    df['Código do Histórico'] = df['Código do Histórico'].astype('int')
+    # Use o método replace para substituir os pontos por vírgulas
+    df['Valor'] = df['Valor'].map('{:.2f}'.format).str.replace('.', ',')
+
+    df = df[['Coligada','Partida','Nº Documento','Conta de Débito','Conta de Crédito','Conta de Contra Partida','Valor','Código do Histórico','Complemento do Histórico','Filial','Centro de Custo']]
+    
+    coligadas = sorted(list(df['Coligada'].unique()))
+    
+    # Criar um arquivo ZIP para salvar os CSVs
+    with zipfile.ZipFile('dados.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for x in coligadas:
+            df_filtrado = df[df['Coligada'] == x].copy()  # Crie uma cópia do DataFrame filtrado
+            df_filtrado.drop(columns=['Coligada'], inplace=True)
+            dados = []
+            for indice, linha in df_filtrado.iterrows():
+                lista_valores = linha.tolist()
+                dados.append(lista_valores)
+
+            # Nome do arquivo CSV que vamos salvar
+            nome_do_arquivo = f'Col{x}' + '_import.csv'
+
+            # Escreva o cabeçalho no arquivo CSV
+            with open(nome_do_arquivo, 'w', newline='') as arquivo_csv:
+                escritor_csv = csv.writer(arquivo_csv, delimiter=';')
+                escritor_csv.writerow(cabecalho)
+
+            # Abrir o arquivo novamente para adicionar os dados
+            with open(nome_do_arquivo, 'a', newline='') as arquivo_csv:
+                escritor_csv = csv.writer(arquivo_csv, delimiter=';')
+                escritor_csv.writerows(dados)
+            
+            # Adicione o arquivo CSV ao arquivo ZIP
+            zipf.write(nome_do_arquivo, arcname=nome_do_arquivo)
+            
+    return 'dados.zip'  # Retorna o nome do arquivo ZIP gerado
 
 def transforma_dados(df, cabecalho):
     df['Partida'] = '*P'
@@ -9,7 +56,6 @@ def transforma_dados(df, cabecalho):
     df = df.dropna()
     # Use o método replace para substituir os pontos por vírgulas
     df['Valor'] = df['Valor'].map('{:.2f}'.format).str.replace('.', ',')
-
     df = df[['Partida','Nº Documento','Conta de Débito','Conta de Crédito','Conta de Contra Partida','Valor','Código do Histórico','Complemento do Histórico','Filial','Centro de Custo']]
 
     dados = []
